e9a6905c2b3d9a0112ea388f153679f30a67ef35
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2024-04-22 21:28:36 +0300

    Update shooting_range.ino

--- shooting_range.ino
+++ shooting_range.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 
-constexpr uint8_t PIN_RS = 4;
-constexpr uint8_t PIN_EN = 5;
+constexpr uint8_t PIN_RS = 6;
+constexpr uint8_t PIN_EN = 7;
 constexpr uint8_t PIN_DB4 = 10;
 constexpr uint8_t PIN_DB5 = 11;
 constexpr uint8_t PIN_DB6 = 12;
@@ -10,13 +10,12 @@ constexpr uint8_t PIN_DB7 = 13;
 LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_DB4, PIN_DB5, PIN_DB6, PIN_DB7);
 
 const int photoresistorPin = A0;
-const int threshold = 65; // Пороговое значение для определения попадания света
+const int threshold = 90; // Пороговое значение для определения попадания света
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.setCursor(0, 1);
 }
 
 void loop() {
@@ -25,5 +24,6 @@ void loop() {
   if (lightValue < threshold) { 
     lcd.print("HEO 1998"); 
     delay(30000);
+    lcd.clear();
   }
 }
